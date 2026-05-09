028d288fc42252e070fe1d3798488c1be87fb5c9
Tanmaybhoyar <tanmaybhoyar524@gmail.com>
2026-05-09 17:22:16 +0530

    commit changes

--- backend/config/db.js
+++ backend/config/db.js
@@ -1,9 +1,16 @@
 const { Sequelize } = require("sequelize");
 require("dotenv").config();
 
+console.log("DATABASE_URL =", process.env.DATABASE_URL);
+
 const sequelize = new Sequelize(process.env.DATABASE_URL, {
   dialect: "mysql",
   logging: false,
 });
+
+sequelize
+  .authenticate()
+  .then(() => console.log("✅ Database connected"))
+  .catch((err) => console.log("❌ DB Error:", err));
 
 module.exports = sequelize;
