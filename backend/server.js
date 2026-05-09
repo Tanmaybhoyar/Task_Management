950b012655c26bed99830613b43c234d62e6fabc
Tanmaybhoyar <tanmaybhoyar524@gmail.com>
2026-05-09 18:12:19 +0530

    commit changes

--- backend/server.js
+++ backend/server.js
@@ -11,7 +11,13 @@ const taskRoutes = require("./routes/taskRoutes");
 
 const app = express();
 
-app.use(cors());
+app.use(
+  cors({
+    origin: "https://task-production-8d25.up.railway.app",
+    credentials: true,
+  })
+);
+
 app.use(express.json());
 
 app.get("/", (req, res) => {
@@ -22,19 +28,8 @@ app.use("/api/auth", authRoutes);
 app.use("/api/projects", projectRoutes);
 app.use("/api/tasks", taskRoutes);
 
-sequelize.authenticate()
-  .then(() => {
-    console.log("Database connected");
-
-    return sequelize.sync();
-  })
-  .then(() => {
-    console.log("Database synced");
-
-    app.listen(process.env.PORT || 5000, () => {
-      console.log(`Server running on port ${process.env.PORT}`);
-    });
-  })
-  .catch((err) => {
-    console.error("Startup Error:", err);
+sequelize.sync().then(() => {
+  app.listen(process.env.PORT || 5000, "0.0.0.0", () => {
+    console.log("Server running");
   });
+});
